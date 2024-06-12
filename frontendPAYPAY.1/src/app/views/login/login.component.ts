5dfd4f134a4cd73e74eddaefd453d3bee8b84e09
Alminagorda <134451182+Alminagorda@users.noreply.github.com>
2024-06-11 21:25:22 -0500

    nuevo cambio de nuevo

--- frontendPAYPAY.1/src/app/views/login/login.component.ts
+++ frontendPAYPAY.1/src/app/views/login/login.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { NavegarComponent } from '../../components/navegar/navegar.component';
-
+import { LoginService } from '../../services/login.service';
+import { JwtRequest } from '../../models/jwtRequest';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -12,16 +18,35 @@ import { NavegarComponent } from '../../components/navegar/navegar.component';
     RouterLink,
     FooterComponent,
     HeaderComponent,
-    NavegarComponent],
+    NavegarComponent,FormsModule,MatFormFieldModule,MatButtonModule,MatInputModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
-  //constructor(private authGoogleService: AuthGoogleService) { }
-  
-  //login() {
-  //  this.authGoogleService.login();
-  //}
+export class LoginComponent implements OnInit {
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
+  username: string = '';
+  password: string = '';
+  mensaje: string = '';
+  ngOnInit(): void {}
+  login() {
+    let request = new JwtRequest();
+    request.username = this.username;
+    request.password = this.password;
+    this.loginService.login(request).subscribe(
+      (data: any) => {
+        sessionStorage.setItem('token', data.jwttoken);
+        this.router.navigate(['homes']);
+      },
+      (error) => {
+        this.mensaje = 'Credenciales incorrectas!!!';
+        this.snackBar.open(this.mensaje, 'Aviso', { duration: 2000 });
+      }
+    );
+  }
 }
 
 
